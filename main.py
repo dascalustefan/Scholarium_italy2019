b95f4d70ada048f647f2b7d5832edcd8e29ca0ab
Dascalu Stefan <st3fandascalu@gmail.com>
2019-04-26 16:54:01 +0300

    added project

---
+++ main.py
@@ -0,0 +1,21 @@
+from app import app
+import secrets
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+"""
+
+This module contains the configurations of the flask application.
+
+"""
+
+# the secret key - used by the browser to keep track of the sessions
+app.secret_key = secrets.token_bytes(32)
+# the database configurations
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://user:password@localhost/scholarium_db'
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+
+if __name__ == '__main__':
+   app.run()
